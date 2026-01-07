b240c35fcc97b62d09999ca2a5047744e57c31ee
MikeJohnnyP <tahoangphuc1901@gmail.com>
2026-01-07 17:45:55 +0700

    Initial commit for first windows dotfiles

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+$vsPath = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationpath
+
+Write-Host "Microsoft Visual Studio path = '$vsPath'"
+
+# Use module `Microsoft.VisualStudio.DevShell.dll`
+Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
+
+Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments '-arch=x64'
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
+
+Import-Module posh-git
+
+Clear-Host
+
+# Force Fastfetch to use YOUR config every time (bypass path confusion)
+if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
+    fastfetch -c "$HOME/.config/fastfetch/config.jsonc"
+}
